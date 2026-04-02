77196dae772626864ccbaf2a5377522eada37955
karmakarmeghdip <karmakarmeghdip@gmail.com>
2026-04-02 19:35:10 +0530

    feat: base commit

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    { nixpkgs, systems, ... }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
+      pkgsFor = nixpkgs.legacyPackages;
+    in
+    {
+      devShells = eachSystem (
+        system:
+        let
+          pkgs = pkgsFor.${system};
+          dlopenLibraries = with pkgs; [
+            libxkbcommon
+
+            # GPU backend
+            vulkan-loader
+            # libGL
+
+            # Window system
+            wayland
+          ];
+        in
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              cargo
+              rustc
+            ];
+
+            # additional libraries that your project
+            # links to at build time, e.g. OpenSSL
+            buildInputs = [ ];
+
+            env.RUSTFLAGS = "-C link-arg=-Wl,-rpath,${nixpkgs.lib.makeLibraryPath dlopenLibraries}";
+          };
+        }
+      );
+    };
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod core;
+mod ui;
+
+fn main() {
+    // iced::run(, view)
+}
